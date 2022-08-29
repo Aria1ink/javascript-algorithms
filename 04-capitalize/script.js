424601c8e3821b1f08a8cf50b1be248312ab5539
Aria1ink <ann.arialink@yandex.ru>
2022-08-29 18:15:37 +0300

    task 4

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -7,7 +7,15 @@
 */
 
 function capitalize(str) {
-    // Напишите код здесь
+  if (str && str != ' ') {
+    let arr = str.trim().split(/\s+/);
+    let strOut = '';
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].replace(arr[i][0], arr[i][0].toUpperCase());
+      strOut += arr[i] + ' ';
+    };
+    return strOut.trim();
+  } else return '';
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
