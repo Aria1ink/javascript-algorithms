98bf1542f3879be62c2cd03a0db5baa40ab477ad
Aria1ink <ann.arialink@yandex.ru>
2022-08-31 23:00:31 +0300

    names

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -8,13 +8,13 @@
 */
 
 function intersection(arr1, arr2) {
-  let arr = [];
+  let result = [];
   for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
-      arr.push(arr1[i]);
+    if (arr2.includes(arr1[i]) && !result.includes(arr1[i])) {
+      result.push(arr1[i]);
     };
   };
-  return arr;
+  return result;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
