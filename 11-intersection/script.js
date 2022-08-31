c5be58bc16adcb8528642c8f35b08f3de834969a
Aria1ink <ann.arialink@yandex.ru>
2022-08-31 16:27:52 +0300

    js task 11

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -8,7 +8,13 @@
 */
 
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+  let arr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
+      arr.push(arr1[i]);
+    };
+  };
+  return arr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
