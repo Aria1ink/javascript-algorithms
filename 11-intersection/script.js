520a2f33833df27dcbec28ba39a168c56142f1c9
Aria1ink <ann.arialink@yandex.ru>
2022-09-01 08:53:32 +0300

    rw1

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -8,7 +8,7 @@
 */
 
 function intersection(arr1, arr2) {
-  let result = [];
+  const result = [];
   for (let i = 0; i < arr1.length; i++) {
     if (arr2.includes(arr1[i]) && !result.includes(arr1[i])) {
       result.push(arr1[i]);
